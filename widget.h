28c54b2b6cbfe6f712fe50ed045ce5d8c544df67
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2001-10-02 22:59:31 +0000

    Added pre/postamble.
    Legacy-ID: 58

--- widget.h
+++ widget.h
@@ -1,3 +1,32 @@
+/* widget.h: Simple dialog boxes for all user interfaces.
+   Copyright (c) 2001 Matan Ziv-Av, Philip Kendall
+
+   $Id$
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+   Author contact information:
+
+   E-mail: pak21-fuse@srcf.ucam.org
+   Postal address: 15 Crescent Road, Wokingham, Berks, RG40 2DB, England
+
+*/
+
+#ifndef FUSE_WIDGET_H
+#define FUSE_WIDGET_H
+
 extern int widget_active;
 
 typedef void (*widget_keyhandler_fn)( int key );
@@ -8,3 +37,5 @@ int widget_init( void );
 int widget_end( void );
 
 const char* widget_selectfile(void);
+
+#endif				/* #ifndef FUSE_WIDGET_H */
