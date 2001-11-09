0436198159ab23b52ede1587b4ee106f8c6ae29f
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2001-11-09 10:02:28 +0000

    Detect whether a file is a .tzx file from its filename.
    Legacy-ID: 118

--- tape.c
+++ tape.c
@@ -111,13 +111,29 @@ int tape_open( const char *filename )
     return 1;
   }
 
-  error = libspectrum_tzx_create( &tape, buffer, file_info.st_size );
-  if( error != LIBSPECTRUM_ERROR_NONE ) {
-    fprintf( stderr,
-	     "%s: error from libspectrum_tzx_create whilst reading `%s': %s\n",
-	     fuse_progname, filename, libspectrum_error_message(error) );
-    munmap( buffer, file_info.st_size );
-    return error;
+  if(    strlen( filename ) < 4
+      || strncmp( &filename[ strlen(filename)-4 ], ".tap", 4 ) ) {
+    
+    error = libspectrum_tzx_create( &tape, buffer, file_info.st_size );
+    if( error != LIBSPECTRUM_ERROR_NONE ) {
+      fprintf( stderr,
+	       "%s: error from libspectrum_tzx_create whilst reading `%s': %s\n",
+	       fuse_progname, filename, libspectrum_error_message(error) );
+      munmap( buffer, file_info.st_size );
+      return error;
+    }
+
+  } else {
+
+    error = libspectrum_tap_create( &tape, buffer, file_info.st_size );
+    if( error != LIBSPECTRUM_ERROR_NONE ) {
+      fprintf( stderr,
+	       "%s: error from libspectrum_tap_create whilst reading `%s': %s\n",
+	       fuse_progname, filename, libspectrum_error_message(error) );
+      munmap( buffer, file_info.st_size );
+      return error;
+    }
+
   }
 
   if( munmap( buffer, file_info.st_size ) == -1 ) {
