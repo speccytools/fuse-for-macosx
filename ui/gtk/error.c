792cc1979515d9a14e20845b297ecec13e71426c
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2002-03-15 11:40:16 +0000

    GTK+ ui_error now displays a dialog box, in addition to printing to stderr.
    Legacy-ID: 392

--- ui/gtk/error.c
+++ ui/gtk/error.c
@@ -29,18 +29,54 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <gdk/gdkkeysyms.h>
+#include <gtk/gtk.h>
+
 #include "fuse.h"
+
+#define MESSAGE_MAX_LENGTH 256
 
 int ui_error( const char *format, ... )
 {
   va_list ap;
+  GtkWidget *dialog, *ok_button, *label;
+  char message[ MESSAGE_MAX_LENGTH + 1 ];
 
+  /* Create the message from the given arguments */
   va_start( ap, format );
-
-   fprintf( stderr, "%s: ", fuse_progname );
-  vfprintf( stderr, format, ap );
-
+  vsnprintf( message, MESSAGE_MAX_LENGTH, format, ap );
   va_end( ap );
+
+  /* Print the message to stderr, along with a program identifier */
+  fprintf( stderr, "%s: %s", fuse_progname, message );
+
+  /* Create the dialog box */
+  dialog = gtk_dialog_new();
+  gtk_window_set_title( GTK_WINDOW( dialog ), "Fuse - Error" );
+  
+  /* Add the OK button into the lower half */
+  ok_button = gtk_button_new_with_label( "OK" );
+  gtk_container_add( GTK_CONTAINER( GTK_DIALOG( dialog )->action_area ),
+		     ok_button );
+
+  /* Create a label with that message */
+  label = gtk_label_new( message );
+  gtk_container_add( GTK_CONTAINER( GTK_DIALOG( dialog )->vbox ), label );
+
+  /* Add some ways to finish the dialog box */
+  gtk_signal_connect_object( GTK_OBJECT( ok_button ), "clicked",
+			     GTK_SIGNAL_FUNC( gtk_widget_destroy ),
+			     GTK_OBJECT( dialog ) );
+  gtk_signal_connect( GTK_OBJECT( dialog ), "delete_event",
+		      GTK_SIGNAL_FUNC( gtk_widget_destroy ), (gpointer) NULL );
+  gtk_widget_add_accelerator( ok_button, "clicked",
+			      gtk_accel_group_get_default(),
+			      GDK_Return, 0, 0 );
+  gtk_widget_add_accelerator( ok_button, "clicked",
+			      gtk_accel_group_get_default(),
+			      GDK_Escape, 0, 0 );
+
+  gtk_widget_show_all( dialog );
 
   return 0;
 }
