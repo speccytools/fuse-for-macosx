b81aae2cbf64d7d179f87f0b3f89e62d167def3c
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2003-08-22 11:51:46 +0000

    If emulation is paused, print the 'stop competition mode' message after checking whether emulation was already paused. Prevents fuse_emulation_pause recursing into itself.
    Legacy-ID: 1573

--- fuse.c
+++ fuse.c
@@ -345,16 +345,16 @@ int fuse_emulation_pause(void)
 {
   int error;
 
+  /* If we were already paused, just return. In any case, increment
+     the pause count */
+  if( fuse_emulation_paused++ ) return 0;
+
   /* Stop recording any competition mode RZX file */
   if( rzx_recording && rzx_competition_mode ) {
     ui_error( UI_ERROR_INFO, "Stopping competition mode RZX recording" );
     error = rzx_stop_recording(); if( error ) return error;
   }
       
-  /* If we were already paused, just return. In any case, increment
-     the pause count */
-  if( fuse_emulation_paused++ ) return 0;
-
   /* If we had sound enabled (and hence doing the speed regulation),
      turn it off */
   if( sound_enabled ) sound_end();
