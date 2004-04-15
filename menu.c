4f7b91669b33da142a0472fc26d4495563a6531a
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2004-04-15 20:06:09 +0000

    Add copyright and GPL notice.
    Legacy-ID: 2128

--- menu.c
+++ menu.c
@@ -1,3 +1,29 @@
+/* menu.c: general menu callbacks
+   Copyright (c) 2004 Philip Kendall
+
+   $Id$
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+   Author contact information:
+
+   Philip Kendall <pak21-fuse@srcf.ucam.org>
+   Postal address: 15 Crescent Road, Wokingham, Berks, RG40 2DB, England
+
+*/
+
 #include <config.h>
 
 #include <libspectrum.h>
