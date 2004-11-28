3c30cfb250cf38f84f4c2572e3b82dee2790bbd7
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2004-11-28 22:22:19 +0000

    Split if1_port_{in,out} into multiple functions.
    Legacy-ID: 2439

--- if1.c
+++ if1.c
@@ -37,11 +37,6 @@
 #include "settings.h"
 #include "ui/ui.h"
 
-#define PORT_MDR 0
-#define PORT_CTR 1
-#define PORT_NET 2
-#define PORT_UNK 0xffff
-
 #define RS232_RAW 0
 #define RS232_INT 1
 
@@ -134,6 +129,13 @@ enum if1_menu_item {
   UMENU_MDRV8,
   UMENU_RS232,
 
+};
+
+enum if1_port {
+  PORT_MDR,
+  PORT_CTR,
+  PORT_NET,
+  PORT_UNKNOWN,
 };
 
 static void
@@ -284,7 +286,7 @@ microdrives_reset( void )
 
 }
 
-static libspectrum_word
+static enum if1_port
 decode_port( libspectrum_word port )
 {
     port &= 0x0018;
@@ -296,124 +298,296 @@ decode_port( libspectrum_word port )
     case 0x0010:
       return PORT_NET;
     default:
-      return PORT_UNK;
+      return PORT_UNKNOWN;
     }
 }
 
-libspectrum_byte
-if1_port_in( libspectrum_word port GCC_UNUSED, int *attached )
+static libspectrum_byte
+port_mdr_in( void )
 {
-
   libspectrum_byte ret = 0xff;
-  int m;
+  size_t i;
 
-  if( !if1_active ) return ret;
+  for( i = 0; i < 8; i++ ) {
 
-  *attached = 1;
+    microdrv_t *drive = &microdrv[ i ];
 
-  port = decode_port( port );
+    if( drive->motor_on && drive->inserted ) {
 
-  /* allow access to the port only if motor 1 is ON and there's a file open */
-  if( port == PORT_MDR ) {
-    for( m = 0; m < 8; m++ ) 
-      if( microdrv[m].motor_on && microdrv[m].inserted ) {
-        if( microdrv[m].transfered < microdrv[m].max_bytes) {
-          microdrv[m].last = microdrv[m].cartridge[microdrv[m].head_pos];
-          increment_head( m );
-        }
-        microdrv[m].transfered++;
-        ret &= microdrv[m].last;  /* I assume negative logic, but how know? */
+      if( drive->transfered < drive->max_bytes ) {
+	drive->last = drive->cartridge[ drive->head_pos ];
+	increment_head( i );
       }
-  }
 
-  if( port == PORT_CTR ) {
-    for( m = 0; m < 8; m++ ) {
-      if( microdrv[m].motor_on && microdrv[m].inserted ) {
-	if(microdrv[m].gap) {
-	  /* ret &= 0xff;  GAP and SYNC high */
-	  microdrv[m].gap--;
-	} else {
-	  ret &= 0xf9; /* GAP and SYNC low */
-	  if( microdrv[m].nogap )
-	    microdrv[m].nogap--;
-	  else {
-	    microdrv[m].gap = 15;
-	    microdrv[m].nogap = 15;
-	  }
-	}
-	if( microdrv[m].wp ) /* if write protected */
-	  ret &= 0xfe; /* active bit */
-      }
+      drive->transfered++;
+      ret &= drive->last;  /* I assume negative logic, but how know? */
     }
-    /* Here we have to poll, the if1_ula 'line' dtr flag. That time it come
-       from the if1_ula.fd_t status :-) */
-    if( if1_ula.dtr == 0 )
-      ret &= 0xf7;	/* %11110111 */
 
-    /* Here we have to poll, the 'SinclairNet' busy flag but never used by 
-       software in Interface 1 */
-    if( if1_ula.busy == 0 )
-      ret &= 0xef;	/* %11101111 */
-/*    fprintf( stderr, "Read CTR ( %%%d%d%d%d%d%d%d%d ).\n", 
-	!!(ret & 128), !!(ret & 64), !!(ret & 32), !!(ret & 16),
-	!!(ret & 8), !!(ret & 4), !!(ret & 2), !!(ret & 1)); */
-    microdrives_restart();
-  }
-
-  if( port == PORT_NET ) {
-/*  fprintf( stderr, "Read PORT NET .. " );*/
-    if( if1_ula.rs232_mode == RS232_RAW ) {		/* if we do raw */
-        /* Here is the input routine */
-	read( if1_ula.fd_r, &if1_ula.tx, 1 );	/* Ok, if no byte, we send last*/
-    } else if( if1_ula.rs232_mode == RS232_INT ) {	/* if we do interpreted */
-        /* Here is the input routine */
-      if( if1_ula.cts ) {	/* If CTS == 1 */
-	if( if1_ula.count_in == 0 ) {
-	  if( if1_ula.fd_r >= 0 && read( if1_ula.fd_r, &if1_ula.data_in, 1 ) == 1 ) {
-	    if1_ula.count_in++;	/* Ok, if read a byte, we begin */
-/*	    fprintf( stderr, "Receive: %d\n", if1_ula.data_in ); */
-	  }
-	  if1_ula.tx = 0;				/* send __ later we raise :-) */
-	} else if( if1_ula.count_in >= 1 && if1_ula.count_in < 5 ) {
-	  if1_ula.tx = 1;				/* send ~~ (start bit :-) */
-	  if1_ula.count_in++;
-	} else if( if1_ula.count_in >= 5 && if1_ula.count_in < 13 ) {
-	  if1_ula.tx = ( if1_ula.data_in & 0x01 ) ? 0 : 1;
-		/*				 send .. (data bits :-) */
-	  if1_ula.data_in >>= 1;		/* prepare next bit :-) */
-	  if1_ula.count_in++;
-	} else 
-	  if1_ula.count_in = 0;
-      } else {	/* if( if1_ula.cts ) */
-        if1_ula.count_in = 0;		/* reset serial in */
-	if1_ula.tx = 0;			/* send __ stop bits or s.e. :-) */
-      }
-    }
-    if( if1_ula.s_net_mode == S_NET_RAW ) {		/* if we do raw */
-        /* Here is the input routine */
-	read( if1_ula.fd_net, &if1_ula.net_i, 1 );	/* Ok, if no byte, we send last*/
-#ifdef IF1_DEBUGX
-	fprintf( stderr, "Received: %d\n", if1_ula.net_o );
-#endif
-    } else if( if1_ula.s_net_mode == S_NET_INT ) {	/* if we do interpreted */
-        /* Here is the input routine */
-    }
-    if( !if1_ula.tx )
-      ret &= 0x7f;
-    if( !if1_ula.net_i )
-      ret &= 0xfe;
-    microdrives_restart();
   }
 
   return ret;
 }
 
+static libspectrum_byte
+port_ctr_in( void )
+{
+  libspectrum_byte ret = 0xff;
+  size_t i;
+
+  for( i = 0; i < 8; i++ ) {
+
+    microdrv_t *drive = &microdrv[ i ];
+
+    if( drive->motor_on && drive->inserted ) {
+      if( drive->gap ) {
+	/* ret &= 0xff;  GAP and SYNC high */
+	drive->gap--;
+      } else {
+	ret &= 0xf9; /* GAP and SYNC low */
+	if( drive->nogap ) {
+	  drive->nogap--;
+	} else {
+	  drive->gap = 15;
+	  drive->nogap = 15;
+	}
+      }
+      if( drive->wp ) /* if write protected */
+	ret &= 0xfe; /* active bit */
+    }
+  }
+  /* Here we have to poll, the if1_ula 'line' dtr flag. That time it come
+     from the if1_ula.fd_t status :-) */
+  if( if1_ula.dtr == 0 )
+    ret &= 0xf7;	/* %11110111 */
+
+  /* Here we have to poll, the 'SinclairNet' busy flag but never used by 
+     software in Interface 1 */
+  if( if1_ula.busy == 0 )
+    ret &= 0xef;	/* %11101111 */
+  /*    fprintf( stderr, "Read CTR ( %%%d%d%d%d%d%d%d%d ).\n", 
+	!!(ret & 128), !!(ret & 64), !!(ret & 32), !!(ret & 16),
+	!!(ret & 8), !!(ret & 4), !!(ret & 2), !!(ret & 1)); */
+  microdrives_restart();
+
+  return ret;
+}
+
+static libspectrum_byte
+port_net_in( void )
+{
+  libspectrum_byte ret = 0xff;
+
+  /*  fprintf( stderr, "Read PORT NET .. " );*/
+  if( if1_ula.rs232_mode == RS232_RAW ) {		/* if we do raw */
+    /* Here is the input routine */
+    read( if1_ula.fd_r, &if1_ula.tx, 1 );	/* Ok, if no byte, we send last*/
+  } else if( if1_ula.rs232_mode == RS232_INT ) {	/* if we do interpreted */
+    /* Here is the input routine */
+    if( if1_ula.cts ) {	/* If CTS == 1 */
+      if( if1_ula.count_in == 0 ) {
+	if( if1_ula.fd_r >= 0 && read( if1_ula.fd_r, &if1_ula.data_in, 1 ) == 1 ) {
+	  if1_ula.count_in++;	/* Ok, if read a byte, we begin */
+	  /*	    fprintf( stderr, "Receive: %d\n", if1_ula.data_in ); */
+	}
+	if1_ula.tx = 0;				/* send __ later we raise :-) */
+      } else if( if1_ula.count_in >= 1 && if1_ula.count_in < 5 ) {
+	if1_ula.tx = 1;				/* send ~~ (start bit :-) */
+	if1_ula.count_in++;
+      } else if( if1_ula.count_in >= 5 && if1_ula.count_in < 13 ) {
+	if1_ula.tx = ( if1_ula.data_in & 0x01 ) ? 0 : 1;
+	/*				 send .. (data bits :-) */
+	if1_ula.data_in >>= 1;		/* prepare next bit :-) */
+	if1_ula.count_in++;
+      } else 
+	if1_ula.count_in = 0;
+    } else {	/* if( if1_ula.cts ) */
+      if1_ula.count_in = 0;		/* reset serial in */
+      if1_ula.tx = 0;			/* send __ stop bits or s.e. :-) */
+    }
+  }
+  if( if1_ula.s_net_mode == S_NET_RAW ) {		/* if we do raw */
+    /* Here is the input routine */
+    read( if1_ula.fd_net, &if1_ula.net_i, 1 );	/* Ok, if no byte, we send last*/
+#ifdef IF1_DEBUGX
+    fprintf( stderr, "Received: %d\n", if1_ula.net_o );
+#endif
+  } else if( if1_ula.s_net_mode == S_NET_INT ) {	/* if we do interpreted */
+    /* Here is the input routine */
+  }
+  if( !if1_ula.tx )
+    ret &= 0x7f;
+  if( !if1_ula.net_i )
+    ret &= 0xfe;
+  microdrives_restart();
+
+  return ret;
+}
+
+libspectrum_byte
+if1_port_in( libspectrum_word port GCC_UNUSED, int *attached )
+{
+  libspectrum_byte ret = 0xff;
+
+  if( !if1_active ) return ret;
+
+  *attached = 1;
+
+  switch( decode_port( port ) )
+  {
+  case PORT_MDR: ret &= port_mdr_in(); break;
+  case PORT_CTR: ret &= port_ctr_in(); break;
+  case PORT_NET: ret &= port_net_in(); break;
+  case PORT_UNKNOWN: break;
+  }
+
+  return ret;
+}
+
+static void
+port_mdr_out( libspectrum_byte val )
+{
+  size_t i;
+
+  /* allow access to the port only if motor 1 is ON and there's a file open */
+  for( i = 0; i < 8; i++ ) {
+
+    microdrv_t *drive = &microdrv[ i ];
+
+    if( drive->motor_on && drive->inserted ) {
+      if( drive->transfered > 11 &&
+	  drive->transfered < drive->max_bytes + 12 ) {
+	drive->cartridge[drive->head_pos] = val;			
+	increment_head( i );
+	drive->modified = 1;
+      }
+      drive->transfered++;
+    }
+  }
+}
+
+static void
+port_ctr_out( libspectrum_byte val )
+{
+  size_t m;
+
+  if( !( val & 0x02 ) && ( if1_ula.comms_clk ) ) {	/* ~~\__ */
+    for( m = 7; m > 0; m-- )
+      microdrv[m].motor_on = microdrv[m - 1].motor_on; /* rotate one drive */
+    microdrv[0].motor_on = (val & 0x01) ? 0 : 1;
+    if( microdrv[0].motor_on || microdrv[1].motor_on || 
+	microdrv[2].motor_on || microdrv[3].motor_on ||
+	microdrv[4].motor_on || microdrv[5].motor_on ||
+	microdrv[6].motor_on || microdrv[7].motor_on ) {
+      if( !if1_mdr_status ) {
+	ui_statusbar_update( UI_STATUSBAR_ITEM_MICRODRV,
+	                     UI_STATUSBAR_STATE_ACTIVE );
+	if1_mdr_status = 1;
+      }
+    } else if ( if1_mdr_status ) {
+      ui_statusbar_update( UI_STATUSBAR_ITEM_MICRODRV,
+			   UI_STATUSBAR_STATE_INACTIVE );
+      if1_mdr_status = 0;
+    }
+  }
+  if( val & 0x01 ) {	/* comms_data == 1 */
+    /* Interface 1 service manual p.:1.4 par.: 1.5.1
+       The same pin on IC1 (if1 ULA), pin 33, is used for the network
+       transmit data and for the RS232 transmit data. In order to select
+       the required function IC1 uses its COMMS_OUT (pin 30) signal,
+       borrowed from the microdrive control when the microdrive is not
+       being used (I do not know what it is exactly meaning. It is a
+       hardware not being used, or a software should not use..?) This signal
+       is routed from pin 30 to the emitter of transistor Q3 (RX DATA) and
+       via resistor R4, to the base of transistor Q1 (NET). When COMMS_OUT
+       is high Q3 is enabled this selecting RS232, and when it is low
+       Q1 is enabled selecting the network.
+	 
+       OK, the schematics offer a different interpretation, because if
+       COMMS_OUT pin level high (>+3V) then Q3 is off (the basis cannot
+       be more higher potential then emitter (NPN transistor), so wathever
+       is on the IC1 RX DATA (pin 33) the basis of Q4 is always on +12V,
+       so the collector of Q4 (PNP transistor) always on -12V.
+       If COMMS_OUT pin level goes low (~0V), then Q3 basis (connected
+       to IC1 RX DATA pin) can be higher level (>+3V) than emitter, so
+       the basis potential of Q4 depend on IC1 RX DATA.
+	 
+       OK, Summa summarum I assume that, the COMMS OUT pin is a
+       negated output of the if1 ULA CTR register's COMMS DATA bit. 
+    */
+    /* C_DATA = 1 */
+    if( if1_ula.comms_data == 0 ) {
+      if1_ula.count_out = 0;
+      if1_ula.data_out = 0;
+      if1_ula.count_in = 0;
+      if1_ula.data_in = 0;
+      /*        if1_ula.status = LINE_RS232; */
+    }
+    /*    } else {	 ( val & 0x01 )  comms_data == 0 
+	  if1_ula.status = LINE_SINCLAIR; */
+  }
+  if1_ula.cts = ( val & 0x10 ) ? 1 : 0;
+  if1_ula.wait = ( val & 0x20 ) ? 1 : 0;
+  if1_ula.comms_data = ( val & 0x01 ) ? 1 : 0;
+  if1_ula.comms_clk = ( val & 0x02 ) ? 1 : 0;
+    
+#ifdef IF1_DEBUG
+  fprintf( stderr, "Set CTS to %d, set WAIT to %d and COMMS_DATA to %d\n",
+	   if1_ula.cts, if1_ula.wait, if1_ula.comms_data );
+#endif
+
+  microdrives_restart();
+}
+
+void
+port_net_out( libspectrum_byte val )
+{
+  if( if1_ula.comms_data == 1 ) {	/* OK, if the comms_data == 1 */
+    if( if1_ula.rs232_mode == RS232_INT ) {	/* if we out byte by byte, do it */
+      if( if1_ula.count_out == 0 ) {	/* waiting for up edge __/~~ */
+	if( if1_ula.rx == 0 && ( val & 0x01 ) == 1 ) {
+	  if1_ula.count_out++;		/* get the start bit __/~~ */
+	}
+      } else if( if1_ula.count_out >= 1 && if1_ula.count_out < 9 ) {
+	if1_ula.data_out >>= 1;
+	if1_ula.data_out |= val & 0x01 ? 0 : 128;
+	if1_ula.count_out++;		/* waiting for next data bit */
+      } else if( if1_ula.count_out >= 9 && if1_ula.count_out < 10 ) {
+	if1_ula.count_out++;		/* waiting for the 1st stop bits */
+      } else if( if1_ula.count_out >= 10 ) {	/* The second stopbit */
+        /* Here is the output routine */
+	if( if1_ula.fd_t > -1 ) {
+	  do ; while( write( if1_ula.fd_t, &if1_ula.data_out, 1 ) != 1 );
+	  /*	    fprintf( stderr, "Send: %d\n", if1_ula.data_out );  */
+	}
+	if1_ula.count_out = 0;
+	if1_ula.data_out = 0;
+      }
+      if1_ula.rx = val & 0x01;		/* set rx */
+    } else { /*if( if1_ula.rs232_mode == RS232_RAW )  if we out bit by bit, do it */
+      /* Here is the output routine for RAW communication */
+      if1_ula.rx = val & 0x01;		/* set rx */
+      write( if1_ula.fd_t, &if1_ula.rx, 1 );
+    }
+  } else {	/* if( if1_ula.comms_data == 1 ) SinclairNET :-)*/
+    if( if1_ula.s_net_mode == S_NET_INT ) {	/* if we out byte by byte, do it */
+
+      if1_ula.net_o = val & 0x01;		/* set rx */
+    } else { /* if( if1_ula.s_net_mode == S_NET_RAW )  if we out bit by bit, do it */
+      /* Here is the output routine */
+      if1_ula.net_o = val & 0x01;		/* set rx */
+      write( if1_ula.fd_net, &if1_ula.net_o, 1 );
+      lseek( if1_ula.fd_net, 1, SEEK_CUR );	/* This is MY bit :-) */
+#ifdef IF1_DEBUG
+      fprintf( stderr, "Send SinclairNET: %d\n", if1_ula.net_o );
+#endif
+    }
+  }
+  microdrives_restart();
+}
+
 void
 if1_port_out( libspectrum_word port GCC_UNUSED, libspectrum_byte val )
 {
-
-  int m;
-  
   if( !if1_active ) return;
 
 #ifdef IF1_DEBUG
@@ -421,136 +595,12 @@ if1_port_out( libspectrum_word port GCC_UNUSED, libspectrum_byte val )
 	!!(val & 128), !!(val & 64), !!(val & 32), !!(val & 16),
 	!!(val & 8), !!(val & 4), !!(val & 2), !!(val & 1), port);
 #endif
-  port = decode_port( port );
 
-
-  /* allow access to the port only if motor 1 is ON and there's a file open */
-	
-  if( port == PORT_MDR ) {
-    for( m = 0; m < 8; m++ ) 
-      if( microdrv[m].motor_on && microdrv[m].inserted ) {
-	if( microdrv[m].transfered > 11 &&
-	    microdrv[m].transfered < microdrv[m].max_bytes + 12 ) {
-	  microdrv[m].cartridge[microdrv[m].head_pos] = val;			
-	  increment_head( m );
-	  microdrv[m].modified = 1;
-	}
-	microdrv[m].transfered++;
-      }
-  }	
-
-  if( port == PORT_CTR ) {
-    if( !( val & 0x02 ) && ( if1_ula.comms_clk ) ) {	/* ~~\__ */
-      for( m = 7; m > 0; m-- )
-	microdrv[m].motor_on = microdrv[m - 1].motor_on; /* rotate one drive */
-      microdrv[0].motor_on = (val & 0x01) ? 0 : 1;
-      if( microdrv[0].motor_on || microdrv[1].motor_on || 
-          microdrv[2].motor_on || microdrv[3].motor_on ||
-	  microdrv[4].motor_on || microdrv[5].motor_on ||
-	  microdrv[6].motor_on || microdrv[7].motor_on ) {
-        if( !if1_mdr_status ) {
-	  ui_statusbar_update( UI_STATUSBAR_ITEM_MICRODRV,
-	                     UI_STATUSBAR_STATE_ACTIVE );
-	  if1_mdr_status = 1;
-        }
-      } else if ( if1_mdr_status ) {
-        ui_statusbar_update( UI_STATUSBAR_ITEM_MICRODRV,
-	                     UI_STATUSBAR_STATE_INACTIVE );
-	if1_mdr_status = 0;
-      }
-    }
-    if( val & 0x01 ) {	/* comms_data == 1 */
-    /* Interface 1 service manual p.:1.4 par.: 1.5.1
-         The same pin on IC1 (if1 ULA), pin 33, is used for the network
-	 transmit data and for the RS232 transmit data. In order to select
-	 the required function IC1 uses its COMMS_OUT (pin 30) signal,
-	 borrowed from the microdrive control when the microdrive is not
-	 being used (I do not know what it is exactly meaning. It is a
-	 hardware not being used, or a software should not use..?) This signal
-	 is routed from pin 30 to the emitter of transistor Q3 (RX DATA) and
-	 via resistor R4, to the base of transistor Q1 (NET). When COMMS_OUT
-	 is high Q3 is enabled this selecting RS232, and when it is low
-	 Q1 is enabled selecting the network.
-	 
-	 OK, the schematics offer a different interpretation, because if
-	 COMMS_OUT pin level high (>+3V) then Q3 is off (the basis cannot
-	 be more higher potential then emitter (NPN transistor), so wathever
-	 is on the IC1 RX DATA (pin 33) the basis of Q4 is always on +12V,
-	 so the collector of Q4 (PNP transistor) always on -12V.
-	 If COMMS_OUT pin level goes low (~0V), then Q3 basis (connected
-	 to IC1 RX DATA pin) can be higher level (>+3V) than emitter, so
-	 the basis potential of Q4 depend on IC1 RX DATA.
-	 
-	 OK, Summa summarum I assume that, the COMMS OUT pin is a
-	 negated output of the if1 ULA CTR register's COMMS DATA bit. 
-    */
-					    /* C_DATA = 1 */
-      if( if1_ula.comms_data == 0 ) {
-        if1_ula.count_out = 0;
-        if1_ula.data_out = 0;
-        if1_ula.count_in = 0;
-        if1_ula.data_in = 0;
-/*        if1_ula.status = LINE_RS232; */
-      }
-/*    } else {	 ( val & 0x01 )  comms_data == 0 
-      if1_ula.status = LINE_SINCLAIR; */
-    }
-    if1_ula.cts = ( val & 0x10 ) ? 1 : 0;
-    if1_ula.wait = ( val & 0x20 ) ? 1 : 0;
-    if1_ula.comms_data = ( val & 0x01 ) ? 1 : 0;
-    if1_ula.comms_clk = ( val & 0x02 ) ? 1 : 0;
-    
-#ifdef IF1_DEBUG
-      fprintf( stderr, "Set CTS to %d, set WAIT to %d and COMMS_DATA to %d\n",
-                if1_ula.cts, if1_ula.wait, if1_ula.comms_data );
-#endif
-
-    microdrives_restart();
-  }
-
-  if( port == PORT_NET ) {
-    if( if1_ula.comms_data == 1 ) {	/* OK, if the comms_data == 1 */
-      if( if1_ula.rs232_mode == RS232_INT ) {	/* if we out byte by byte, do it */
-        if( if1_ula.count_out == 0 ) {	/* waiting for up edge __/~~ */
-          if( if1_ula.rx == 0 && ( val & 0x01 ) == 1 ) {
-	    if1_ula.count_out++;		/* get the start bit __/~~ */
-	  }
-        } else if( if1_ula.count_out >= 1 && if1_ula.count_out < 9 ) {
-          if1_ula.data_out >>= 1;
-          if1_ula.data_out |= val & 0x01 ? 0 : 128;
-	  if1_ula.count_out++;		/* waiting for next data bit */
-        } else if( if1_ula.count_out >= 9 && if1_ula.count_out < 10 ) {
-	  if1_ula.count_out++;		/* waiting for the 1st stop bits */
-        } else if( if1_ula.count_out >= 10 ) {	/* The second stopbit */
-        /* Here is the output routine */
-	  if( if1_ula.fd_t > -1 ) {
-	    do ; while( write( if1_ula.fd_t, &if1_ula.data_out, 1 ) != 1 );
-/*	    fprintf( stderr, "Send: %d\n", if1_ula.data_out );  */
-	  }
-          if1_ula.count_out = 0;
-          if1_ula.data_out = 0;
-        }
-        if1_ula.rx = val & 0x01;		/* set rx */
-      } else { /*if( if1_ula.rs232_mode == RS232_RAW )  if we out bit by bit, do it */
-        /* Here is the output routine for RAW communication */
-        if1_ula.rx = val & 0x01;		/* set rx */
-        write( if1_ula.fd_t, &if1_ula.rx, 1 );
-      }
-    } else {	/* if( if1_ula.comms_data == 1 ) SinclairNET :-)*/
-      if( if1_ula.s_net_mode == S_NET_INT ) {	/* if we out byte by byte, do it */
-
-        if1_ula.net_o = val & 0x01;		/* set rx */
-      } else { /* if( if1_ula.s_net_mode == S_NET_RAW )  if we out bit by bit, do it */
-        /* Here is the output routine */
-        if1_ula.net_o = val & 0x01;		/* set rx */
-        write( if1_ula.fd_net, &if1_ula.net_o, 1 );
-	lseek( if1_ula.fd_net, 1, SEEK_CUR );	/* This is MY bit :-) */
-#ifdef IF1_DEBUG
-	fprintf( stderr, "Send SinclairNET: %d\n", if1_ula.net_o );
-#endif
-      }
-    }
-    microdrives_restart();
+  switch( decode_port( port ) ) {
+  case PORT_MDR: port_mdr_out( val ); break;
+  case PORT_CTR: port_ctr_out( val ); break;
+  case PORT_NET: port_net_out( val ); break;
+  case PORT_UNKNOWN: break;
   }
 }
 
