fb94e6a9f1e075545ce45bab957ebdd2a8cb023e
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2005-01-02 23:30:32 +0000

    Don't try and set the 'Record movie as PNG' menu item as (in)active if libpng not present.
    Legacy-ID: 2464

--- ui.c
+++ ui.c
@@ -188,7 +188,9 @@ static const struct menu_item_entries menu_item_lookup[] = {
 
   { UI_MENU_ITEM_FILE_MOVIES_RECORDING, "/File/Movies/Stop Movie Recording",
     "/File/Movies/Record Movie as SCR...", 1,
+#ifdef USE_PNG
     "/File/Movies/Record Movie as PNG...", 1,
+#endif
   },
   
   { UI_MENU_ITEM_MEDIA_CARTRIDGE, "/Media/Cartridge" },
