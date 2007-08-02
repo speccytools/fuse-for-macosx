4a1dfb56a6bd5a1b68811d543171db7a242cf1e5
Fredrick Meunier <fredm@spamcop.net>
2007-08-02 13:04:07 +0000

    Correct joystick port mask on TC2048 (verified on real hardware).
    Legacy-ID: 3088

--- machines/tc2048.c
+++ machines/tc2048.c
@@ -47,7 +47,7 @@ static int tc2048_reset( void );
 static libspectrum_byte tc2048_contend_delay( libspectrum_dword time );
 
 static const periph_t peripherals[] = {
-  { 0x00e0, 0x0000, joystick_kempston_read, NULL },
+  { 0x0020, 0x0000, joystick_kempston_read, NULL },
   { 0x0018, 0x0010, if1_port_in, if1_port_out },
   { 0x0018, 0x0008, if1_port_in, if1_port_out },
   { 0x0018, 0x0000, if1_port_in, if1_port_out },
