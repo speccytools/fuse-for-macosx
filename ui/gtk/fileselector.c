a61892f7bd6a90e742330bfbc010379a46fcfb79
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2007-08-06 21:56:22 +0000

    Replace deprecated GtkFileSelection with GtkFileChooserDialog. TODO: reimplement "current directory" functionality.
    Legacy-ID: 3100

--- ui/gtk/fileselector.c
+++ ui/gtk/fileselector.c
@@ -1,5 +1,5 @@
 /* fileselector.c: GTK+ fileselector routines
-   Copyright (c) 2000-2003 Philip Kendall
+   Copyright (c) 2000-2007 Philip Kendall
 
    $Id$
 
@@ -25,124 +25,50 @@
 
 #include <config.h>
 
-#include <errno.h>
-#ifdef HAVE_LIBGEN_H
-#include <libgen.h>
-#endif			/* #ifdef HAVE_LIBGEN_H */
-#include <string.h>
-
-#include <gdk/gdkkeysyms.h>
 #include <gtk/gtk.h>
 
-#include "compat.h"
 #include "gtkinternals.h"
-#include "menu.h"
-#include "ui/ui.h"
 
-/* The last directory we visited */
-static char *cwd = NULL;
-
-static void fileselector_done( GtkButton *button, gpointer user_data );
-static char *fileselector_get_filename( const char *title );
-
-typedef struct fileselector_info {
-
-  GtkWidget *selector;
-  gchar *filename;
-
-} fileselector_info;
-
-static char*
-fileselector_get_filename( const char *title )
-{
-  fileselector_info selector;
-  GtkAccelGroup *accel_group;
-
-  selector.selector = gtk_file_selection_new( title );
-  selector.filename = NULL;
-
-  gtk_signal_connect(
-    GTK_OBJECT( GTK_FILE_SELECTION( selector.selector )->ok_button ),
-    "clicked", GTK_SIGNAL_FUNC( fileselector_done ), &selector
-  );
-
-  gtk_signal_connect_object(
-    GTK_OBJECT( GTK_FILE_SELECTION( selector.selector )->cancel_button ),
-    "clicked", DEFAULT_DESTROY, GTK_OBJECT( selector.selector )
-  );
-
-  gtk_signal_connect( GTK_OBJECT( selector.selector ), "delete-event",
-		      DEFAULT_DESTROY, NULL );
-
-  accel_group = gtk_accel_group_new();
-  gtk_window_add_accel_group( GTK_WINDOW( selector.selector ), accel_group );
-
-  /* Allow Esc to cancel */
-  gtk_widget_add_accelerator(
-    GTK_FILE_SELECTION( selector.selector )->cancel_button, "clicked",
-    accel_group, GDK_Escape, 0, 0
-  );
-
-  gtk_window_set_modal( GTK_WINDOW( selector.selector ), TRUE );
-
-  if( cwd )
-    gtk_file_selection_set_filename( GTK_FILE_SELECTION( selector.selector ),
-				     cwd );
-
-  gtk_widget_show( selector.selector );
-
-  gtk_main();
-
-  return selector.filename;
-}
+/* TODO: reimplement "current directory" functionality */
 
 char*
 menu_get_open_filename( const char *title )
 {
-  return fileselector_get_filename( title );
+  GtkWidget *dialog;
+  char *filename = NULL;
+
+  dialog =
+    gtk_file_chooser_dialog_new( title, GTK_WINDOW( gtkui_window ),
+				 GTK_FILE_CHOOSER_ACTION_OPEN,
+				 GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+				 GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+				 NULL );
+
+  if( gtk_dialog_run( GTK_DIALOG( dialog ) ) == GTK_RESPONSE_ACCEPT )
+    filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER( dialog ) );
+
+  gtk_widget_destroy( dialog );
+
+  return filename;
 }
 
 char*
 menu_get_save_filename( const char *title )
 {
-  return fileselector_get_filename( title );
-}
-
-static void
-fileselector_done( GtkButton *button GCC_UNUSED, gpointer user_data )
-{
-  fileselector_info *ptr = user_data;
-  const char *filename; char *buffer, *buffer2;
-
-  filename =
-    gtk_file_selection_get_filename( GTK_FILE_SELECTION( ptr->selector ) );
-
-  ptr->filename = strdup( filename );
-  if( !ptr->filename ) {
-    ui_error( UI_ERROR_ERROR, "out of memory at %s:%d", __FILE__, __LINE__ );
-    goto end;
-  }
-
-  /* And store a copy of the path so we can return here */
-  buffer = strdup( filename );
-  if( !buffer ) {
-    ui_error( UI_ERROR_ERROR, "out of memory at %s:%d", __FILE__, __LINE__ );
-    goto end;
-  }
-
-  buffer2 = dirname( buffer );
+  GtkWidget *dialog;
+  char *filename = NULL;
   
-  free( cwd );
-  cwd = malloc( strlen( buffer2 ) + 2 );
-  if( !cwd ) {
-    ui_error( UI_ERROR_ERROR, "out of memory at %s:%d", __FILE__, __LINE__ );
-    goto end;
-  }
-   
-  strcpy( cwd, buffer2 ); strcat( cwd, "/" );
-  free( buffer );
+  dialog =
+    gtk_file_chooser_dialog_new( title, GTK_WINDOW( gtkui_window ),
+				 GTK_FILE_CHOOSER_ACTION_SAVE,
+				 GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+				 GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+				 NULL );
 
-  end:
-  gtk_widget_destroy( ptr->selector );
-  gtk_main_quit();
+  if( gtk_dialog_run( GTK_DIALOG( dialog ) ) == GTK_RESPONSE_ACCEPT )
+    filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER( dialog ) );
+
+  gtk_widget_destroy( dialog );
+
+  return filename;
 }
