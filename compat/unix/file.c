af0fdfd5657bbbb4f0ec53a1b05ac351164020fc
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2008-12-10 09:41:48 +0000

    Must open files for writing with O_TRUNC to ensure that any old data is cleared out. Also fix a dependency problem (thanks, Matthew Westcott).
    Legacy-ID: 3893

--- compat/unix/file.c
+++ compat/unix/file.c
@@ -47,7 +47,8 @@ const compat_fd COMPAT_FILE_OPEN_FAILED = -1;
 compat_fd
 compat_file_open( const char *path, int write )
 {
-  int flags = write ? O_WRONLY | O_CREAT | O_BINARY : O_RDONLY | O_BINARY;
+  int flags = O_BINARY;
+  flags |= write ? O_WRONLY | O_CREAT | O_TRUNC : O_RDONLY;
   return open( path, flags, 0666 );
 }
 
