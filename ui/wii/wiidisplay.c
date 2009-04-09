f78af048c42ca980f3f2d291f30850d8ca337297
Marek Januszewski <marekj1@gmail.com>
2009-04-09 00:56:00 +0000

    Wii: Implemented scalers/filters
    Legacy-ID: 3996

--- ui/wii/wiidisplay.c
+++ ui/wii/wiidisplay.c
@@ -1,5 +1,5 @@
 /* wiidisplay.c: Routines for dealing with the Wii's framebuffer display
-   Copyright (c) 2008 Bjoern Giesler
+   Copyright (c) 2008-2009 Bjoern Giesler, Marek Januszewski
 
    $Id$
 
@@ -43,22 +43,41 @@
 #include "settings.h"
 #include "ui/wii/wiimouse.h"
 
+/* The size of a 1x1 image in units of
+   DISPLAY_ASPECT WIDTH x DISPLAY_SCREEN_HEIGHT */
+int image_scale;
+
+/* The height and width of a 1x1 image in pixels */
+int image_width, image_height;
+
 /* A copy of every pixel on the screen */
 libspectrum_word display_image[2 * DISPLAY_SCREEN_HEIGHT][DISPLAY_SCREEN_WIDTH];
 
-/* Are we in a Timex display mode? */
-static int hires;
+/* An RGB image of the Spectrum screen; slightly bigger than the real
+   screen to handle the smoothing filters which read around each pixel */
+static unsigned char rgb_image[ 4 * 2 * ( DISPLAY_SCREEN_HEIGHT + 4 ) *
+                                        ( DISPLAY_SCREEN_WIDTH  + 3 )   ];
+static const int rgb_pitch = ( DISPLAY_SCREEN_WIDTH + 3 ) * 4;
+
+/* The scaled image */
+static unsigned char scaled_image[ 4 * 3 * DISPLAY_SCREEN_HEIGHT *
+                                  (size_t)(1.5 * DISPLAY_SCREEN_WIDTH) ];
+static const ptrdiff_t scaled_pitch = 4 * 1.5 * DISPLAY_SCREEN_WIDTH;
 
 static u32 *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
-static int needsclear = 1;
+/* The current size of the window (in units of DISPLAY_SCREEN_*) */
+static int wiidisplay_current_size=2;
+
+static int init_colours( void );
+static int register_scalers( void );
 
 typedef struct {
   u8 r, g, b;
 } rgb_t;
 
-rgb_t spectrum_colors[16] = {
+rgb_t rgb_colours[16] = {
   /* no bright */
   {0, 0, 0},       /* BLACK */
   {0, 0, 192},     /* BLUE */
@@ -79,7 +98,8 @@ rgb_t spectrum_colors[16] = {
   {255, 255, 255}, /* WHITE */
 };
   
-u32 palette[16][16];
+static u32 wiidisplay_colours[16];
+static u32 bw_colours[16];
 
 #define MOUSESIZEX 9
 #define MOUSESIZEY 9
@@ -117,14 +137,6 @@ u32 convert_rgb (rgb_t rgb1, rgb_t rgb2)
   return (y1 << 24) | (cb << 16) | (y2 << 8) | cr;
 }
 
-void populate_palette(void)
-{
-  u8 i, j;
-  for(i=0; i<16; i++)
-    for(j=0; j<16; j++)
-      palette[i][j] = convert_rgb(spectrum_colors[i], spectrum_colors[j]);
-}
-
 static void put_pixel(int x, int y, int colour, int mouseputpixel)
 {
   if( machine_current->timex ) {
@@ -147,20 +159,134 @@ static int get_pixel(int x, int y)
   return display_image[y][x];
 }
 
+static int
+init_colours( void )
+{
+  size_t i;
+
+  for( i = 0; i < 16; i++ ) {
+
+    rgb_t colour, grey;
+
+    colour = rgb_colours[i];
+
+    /* Addition of 0.5 is to avoid rounding errors */
+    grey.r = grey.g = grey.b =
+      ( 0.299 * colour.r + 0.587 * colour.g + 0.114 * colour.b ) + 0.5;
+
+#ifdef WORDS_BIGENDIAN
+
+    wiidisplay_colours[i] = colour.r << 24 | colour.g << 16 | colour.b << 8;
+            bw_colours[i] = grey.r << 24 |  grey.g << 16 | grey.b << 8;
+
+#else                           /* #ifdef WORDS_BIGENDIAN */
+
+    wiidisplay_colours[i] = colour.r | colour.g << 8 | colour.b << 16;
+            bw_colours[i] = grey.r |  grey.g << 8 | grey.b << 16;
+
+#endif                          /* #ifdef WORDS_BIGENDIAN */
+
+  }
+
+  return 0;
+}
+
 int uidisplay_init(int width, int height)
 {
-  hires = ( width == 640 ? 1 : 0 );
+  int error;
 
-  scaler_register_clear();
-  populate_palette();
+  image_width = width; image_height = height;
+  image_scale = width / DISPLAY_ASPECT_WIDTH;
+
+  error = register_scalers(); if( error ) return error;
+
   display_ui_initialised = 1;
   display_refresh_all();
 
   return 0;
 }
 
+static int
+register_scalers( void )
+{
+  scaler_register_clear();
+
+  switch( wiidisplay_current_size ) {
+
+  case 1:
+
+    switch( image_scale ) {
+    case 1:
+      scaler_register( SCALER_NORMAL );
+      scaler_register( SCALER_PALTV );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_NORMAL );
+      return 0;
+    case 2:
+      scaler_register( SCALER_HALF );
+      scaler_register( SCALER_HALFSKIP );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_HALF );
+      return 0;
+    }
+
+  case 2:
+
+    switch( image_scale ) {
+    case 1:
+      scaler_register( SCALER_DOUBLESIZE );
+      scaler_register( SCALER_TV2X );
+      scaler_register( SCALER_ADVMAME2X );
+      scaler_register( SCALER_2XSAI );
+      scaler_register( SCALER_SUPER2XSAI );
+      scaler_register( SCALER_SUPEREAGLE );
+      scaler_register( SCALER_DOTMATRIX );
+      scaler_register( SCALER_PALTV2X );
+      scaler_register( SCALER_HQ2X );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_DOUBLESIZE );
+      return 0;
+    case 2:
+      scaler_register( SCALER_NORMAL );
+      scaler_register( SCALER_TIMEXTV );
+      scaler_register( SCALER_PALTV );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_NORMAL );
+      return 0;
+    }
+
+  case 3:
+
+    switch( image_scale ) {
+    case 1:
+      scaler_register( SCALER_TRIPLESIZE );
+      scaler_register( SCALER_TV3X );
+      scaler_register( SCALER_ADVMAME3X );
+      scaler_register( SCALER_PALTV3X );
+      scaler_register( SCALER_HQ3X );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_TRIPLESIZE );
+      return 0;
+    case 2:
+      scaler_register( SCALER_TIMEX1_5X );
+      if( !scaler_is_supported( current_scaler ) )
+	scaler_select_scaler( SCALER_TIMEX1_5X );
+      return 0;
+    }
+
+  }
+
+  ui_error( UI_ERROR_ERROR, "Unknown display size/image size %d/%d",
+	    wiidisplay_current_size, image_scale );
+  return 1;
+}
+
 int wiidisplay_init(void)
 {
+  int error;
+        
+  error = init_colours(); if( error ) return error;
+
   VIDEO_Init();
 
   u32 videoMode = VIDEO_GetCurrentTvMode();
@@ -254,56 +380,80 @@ void wiidisplay_showmouse( float x, float y )
 }
 
 void
-uidisplay_area( int x, int start, int width, int height )
+uidisplay_area( int x, int y, int w, int h )
 {
-  int y, i;
+  float scale = (float)wiidisplay_current_size / image_scale;
+  int scaled_x, scaled_y, i, yy;
+  libspectrum_dword *palette;
 
-  int ystart = (rmode->xfbHeight - 2*DISPLAY_SCREEN_HEIGHT)/4;
+  /* Extend the dirty region by 1 pixel for scalers
+     that "smear" the screen, e.g. 2xSAI */
+  if( scaler_flags & SCALER_FLAGS_EXPAND )
+    scaler_expander( &x, &y, &w, &h, image_width, image_height );
 
-  u32 *dest;
+  scaled_x = scale * x; scaled_y = scale * y;
 
-  if( needsclear ) {
-    for( y=0; y<rmode->xfbHeight; y++ ) {
-      for( i=0, dest = xfb + y*rmode->fbWidth/2;
-           i<rmode->fbWidth;
-	   i+=2, dest++ )
-      *dest = palette[0][0];
-    }
-    needsclear = 0;
+  palette = settings_current.bw_tv ? bw_colours : wiidisplay_colours;
+
+  /* Create the RGB image */
+  for( yy = y; yy < y + h; yy++ ) {
+
+    libspectrum_dword *rgb; libspectrum_word *display;
+
+    rgb = (libspectrum_dword*)( rgb_image + ( yy + 2 ) * rgb_pitch );
+    rgb += x + 1;
+
+    display = &display_image[yy][x];
+
+    for( i = 0; i < w; i++, rgb++, display++ ) *rgb = palette[ *display ];
   }
 
-  if( x%2 ) {
-    x -= 1;
-    width += 1;
+  /* Create scaled image */
+  scaler_proc32( &rgb_image[ ( y + 2 ) * rgb_pitch + 4 * ( x + 1 ) ],
+                 rgb_pitch,
+                 &scaled_image[ scaled_y * scaled_pitch + 4 * scaled_x ],
+                 scaled_pitch, w, h );
+
+  w *= scale; h *= scale;
+
+  /* Blit to the real screen */
+  int disp_x,disp_y;
+  long ofs;
+  u16 fb_pitch = rmode->fbWidth / 2;
+  /* ystart is an offset to center spectrum screen on wii screen */
+  int ystart = ( rmode->xfbHeight - 2 * DISPLAY_SCREEN_HEIGHT ) / 4;
+  u32 *dest, *next_line;
+
+  if( scaled_x%2 ) {
+    scaled_x -= 1;
+    w += 1;
   }
-  
-  for( y=start; y<start+height; y++ ) {
-    /* FIXME: Black-and-white only */
-    if(hires) {
-      for( i=0, dest = xfb + 2*(y+ystart)*rmode->fbWidth/2 + x/2;
-           i < width;
-	   i+=2, dest++ )
-        *dest = palette[display_image[y][x+i]][display_image[y][x+i+1]];
-      for( i=0, dest = xfb + (2*(y+ystart) + 1)*rmode->fbWidth/2 + x/2;
-	   i < width;
-	   i+=2, dest++ )
-	*dest = palette[display_image[y][x+i]][display_image[y][x+i+1]];
-    } else {
-      for( i=0, dest = xfb + 2*(y+ystart)*rmode->fbWidth/2 + x;
-	   i < width;
-	   i+=2, dest++ ) {
-	*dest = palette[display_image[y][x+i]][display_image[y][x+i]];
-	dest++;
-	*dest = palette[display_image[y][x+i+1]][display_image[y][x+i+1]];
-      }
-      for( i=0, dest = xfb + (2*(y+ystart) + 1)*rmode->fbWidth/2 + x;
-	   i < width;
-	   i+=2, dest++ ) {
-	*dest = palette[display_image[y][x+i]][display_image[y][x+i]];
-	dest++;
-	*dest = palette[display_image[y][x+i+1]][display_image[y][x+i+1]];
-      }
+
+/* FIXME: optimize this procedure and implement double buffering */
+  next_line = xfb + scaled_x / 2 + ( scaled_y + ystart ) * fb_pitch;
+
+  for( disp_y = scaled_y; disp_y < scaled_y + h; disp_y++ ) {
+
+    dest = next_line;
+
+    for( disp_x = scaled_x; disp_x < scaled_x + w; disp_x++ ) {
+      ofs = ( 4 * disp_x ) + ( scaled_pitch * disp_y );
+
+      rgb_t rgb1, rgb2;
+
+      rgb1.b = scaled_image[ ofs + 2 ]; /* blue */
+      rgb1.g = scaled_image[ ofs + 1 ]; /* green */
+      rgb1.r = scaled_image[ ofs + 0 ]; /* red */
+
+      rgb2.b = scaled_image[ ofs + 2 + 4 ]; /* blue */
+      rgb2.g = scaled_image[ ofs + 1 + 4 ]; /* green */
+      rgb2.r = scaled_image[ ofs + 0 + 4 ]; /* red */
+
+      *dest = convert_rgb( rgb1, rgb2 );
+      
+      dest += disp_x%2;
     }
+    next_line += fb_pitch;
   }
 }
 
