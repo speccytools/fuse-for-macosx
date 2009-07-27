ec0a89ccb0bcf1234ce48ae9922f370ef5595fec
Fredrick Meunier <fredm@spamcop.net>
2009-07-27 12:38:06 +0000

    No tape traps when Opus ROM is active.
    Legacy-ID: 4054

--- tape.c
+++ tape.c
@@ -555,6 +555,9 @@ trap_check_rom( void )
   if( plusd_available && plusd_active )
     return 0;		/* +D must not be active */
 
+  if( opus_available && opus_active )
+    return 0;		/* Opus must not be active */
+
   if( memory_custom_rom() )
     return 0;           /* and we can't be using a custom ROM */
 
