829ccc26b12da7ec15b103d7c84be939c468f816
Stuart Brady <sdb@zubnet.me.uk>
2011-04-01 21:51:56 +0000

    Update copyright dates
    Legacy-ID: 4334

--- fuse.c
+++ fuse.c
@@ -402,7 +402,7 @@ static void fuse_show_copyright(void)
   printf( "\n" );
   fuse_show_version();
   printf(
-   "Copyright (c) 1999-2010 Philip Kendall and others; see the file\n"
+   "Copyright (c) 1999-2011 Philip Kendall and others; see the file\n"
    "'AUTHORS' for more details.\n"
    "\n"
    "For help, please mail <fuse-emulator-devel@lists.sf.net> or use\n"
