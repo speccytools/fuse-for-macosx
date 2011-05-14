5ca9610ecf1ac6c67c88e3218ba04edc270c7ac0
Fredrick Meunier <fredm@spamcop.net>
2011-05-14 14:22:24 +0000

    We want to allow 0 tstate blocks to twiddle the mic to accommodate the set signal level block.
    Legacy-ID: 4438

--- tape.c
+++ tape.c
@@ -835,7 +835,10 @@ tape_next_edge( libspectrum_dword last_tstates, int type, void *user_data )
   if( libspec_error != LIBSPECTRUM_ERROR_NONE ) return;
 
   /* Invert the microphone state */
-  if( edge_tstates || ( flags & LIBSPECTRUM_TAPE_FLAGS_STOP ) ) {
+  if( edge_tstates ||
+      ( flags & ( LIBSPECTRUM_TAPE_FLAGS_STOP |
+                  LIBSPECTRUM_TAPE_FLAGS_LEVEL_LOW |
+                  LIBSPECTRUM_TAPE_FLAGS_LEVEL_HIGH ) ) ) {
 
     if( flags & LIBSPECTRUM_TAPE_FLAGS_NO_EDGE ) {
       /* Do nothing */
