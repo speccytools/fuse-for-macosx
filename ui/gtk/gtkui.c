3247563be29033a6052b75c1d1ad7246d7d6fbdb
Sergio Baldoví <serbalgi@gmail.com>
2015-12-27 23:47:25 +0000

    Update properties used in GtkAboutDialog.
    - Use program-name property rather than name, as it was shown the executable
      name.
    - Add logo-icon-name property as it was shown the icon "image-missing".
    
    Legacy-ID: 5350

--- ui/gtk/gtkui.c
+++ ui/gtk/gtkui.c
@@ -658,10 +658,12 @@ menu_help_keyboard( GtkAction *gtk_action GCC_UNUSED, gpointer data GCC_UNUSED )
 void
 menu_help_about( GtkAction *gtk_action GCC_UNUSED, gpointer data GCC_UNUSED )
 {
+  /* TODO: show Fuse icon */
   gtk_show_about_dialog( GTK_WINDOW( gtkui_window ),
-                         "name", "Fuse",
+                         "program-name", "Fuse",
                          "comments", "The Free Unix Spectrum Emulator",
                          "copyright", FUSE_COPYRIGHT,
+                         "logo-icon-name", NULL,
                          "version", VERSION,
                          "website", PACKAGE_URL,
                          NULL );
