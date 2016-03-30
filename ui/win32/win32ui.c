5fde88b59b5e258a7f4e06f5fa0979c98e3e3d03
Johannes Schickel <lordhoto@scummvm.org>
2016-03-30 21:55:05 +0000

    Fix compilation with MinGW-w64 toolchain.
    Legacy-ID: 5372

--- ui/win32/win32ui.c
+++ ui/win32/win32ui.c
@@ -302,8 +302,15 @@ WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
   fuse_hPrevInstance = hPrevInstance;
 
 /* HACK: __argc, __argv are broken and return zero when using mingwrt 4.0+
-   on MinGW */
-#if defined( __GNUC__ ) && defined( __MINGW32__ ) && !defined( __MINGW64__ )
+   on MinGW.
+   HACK: MinGW-w64 based toolchains neither feature _argc nor _argv. The 32 bit
+   incarnation only defines __MINGW32__. This leads to build breakage due to
+   missing declarations. Luckily MinGW-w64 based toolchains define
+   __MINGW64_VERSION_foo macros inside _mingw.h, which is included from all
+   system headers. Thus we abuse that to detect them.
+*/
+#if defined( __GNUC__ ) && defined( __MINGW32__ ) \
+                        && !defined( __MINGW64_VERSION_MAJOR )
   return fuse_main( _argc, _argv );
 #else
   return fuse_main( __argc, __argv );
