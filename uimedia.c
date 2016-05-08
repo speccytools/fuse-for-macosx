31387c0aab25050150f24d9737665556f25fbc38
Sergio Baldoví <serbalgi@gmail.com>
2016-05-08 18:06:09 +0000

    Remember filename when saving a recently formatted disk.
    Legacy-ID: 5469

--- uimedia.c
+++ uimedia.c
@@ -199,7 +199,8 @@ drive_disk_write( const ui_media_drive_info_t *drive, const char *filename )
     return 1;
   }
 
-  if( drive->fdd->disk.filename && strcmp( filename, drive->fdd->disk.filename ) ) {
+  if( !drive->fdd->disk.filename ||
+      strcmp( filename, drive->fdd->disk.filename ) ) {
     libspectrum_free( drive->fdd->disk.filename );
     drive->fdd->disk.filename = utils_safe_strdup( filename );
   }
