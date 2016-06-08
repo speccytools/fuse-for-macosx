ef00b76ff09176d4946bc613c2a54ff215f003b4
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2016-06-08 22:03:35 +0000

    Expose all Z80 registers as system variables.
    Legacy-ID: 5617

--- z80/z80.c
+++ z80/z80.c
@@ -1,5 +1,5 @@
 /* z80.c: z80 supplementary functions
-   Copyright (c) 1999-2013 Philip Kendall
+   Copyright (c) 1999-2016 Philip Kendall
    Copyright (c) 2015 Stuart Brady
 
    This program is free software; you can redistribute it and/or modify
@@ -86,10 +86,45 @@ static module_info_t z80_module_info = {
 
 /* Debugger system variables */
 static const char * const debugger_type_string = "z80";
+static const char * const a_detail_string = "a";
+static const char * const b_detail_string = "b";
+static const char * const c_detail_string = "c";
+static const char * const d_detail_string = "d";
+static const char * const e_detail_string = "e";
+static const char * const f_detail_string = "f";
+static const char * const h_detail_string = "h";
+static const char * const l_detail_string = "l";
+
+static const char * const a__detail_string = "a'";
+static const char * const b__detail_string = "b'";
+static const char * const c__detail_string = "c'";
+static const char * const d__detail_string = "d'";
+static const char * const e__detail_string = "e'";
+static const char * const f__detail_string = "f'";
+static const char * const h__detail_string = "h'";
+static const char * const l__detail_string = "l'";
+
 static const char * const af_detail_string = "af";
 static const char * const bc_detail_string = "bc";
 static const char * const de_detail_string = "de";
 static const char * const hl_detail_string = "hl";
+
+static const char * const af__detail_string = "af'";
+static const char * const bc__detail_string = "bc'";
+static const char * const de__detail_string = "de'";
+static const char * const hl__detail_string = "hl'";
+
+static const char * const sp_detail_string = "sp";
+static const char * const pc_detail_string = "pc";
+static const char * const ix_detail_string = "ix";
+static const char * const iy_detail_string = "iy";
+
+static const char * const i_detail_string = "i";
+static const char * const r_detail_string = "r";
+
+static const char * const im_detail_string = "im";
+static const char * const iff1_detail_string = "iff1";
+static const char * const iff2_detail_string = "iff2";
 
 static void
 z80_interrupt_event_fn( libspectrum_dword event_tstates, int type,
@@ -118,6 +153,40 @@ set_##reg( libspectrum_dword value ) \
   reg = value; \
 }
 
+DEBUGGER_GET(A)
+DEBUGGER_SET(A)
+DEBUGGER_GET(B)
+DEBUGGER_SET(B)
+DEBUGGER_GET(C)
+DEBUGGER_SET(C)
+DEBUGGER_GET(D)
+DEBUGGER_SET(D)
+DEBUGGER_GET(E)
+DEBUGGER_SET(E)
+DEBUGGER_GET(F)
+DEBUGGER_SET(F)
+DEBUGGER_GET(H)
+DEBUGGER_SET(H)
+DEBUGGER_GET(L)
+DEBUGGER_SET(L)
+
+DEBUGGER_GET(A_)
+DEBUGGER_SET(A_)
+DEBUGGER_GET(B_)
+DEBUGGER_SET(B_)
+DEBUGGER_GET(C_)
+DEBUGGER_SET(C_)
+DEBUGGER_GET(D_)
+DEBUGGER_SET(D_)
+DEBUGGER_GET(E_)
+DEBUGGER_SET(E_)
+DEBUGGER_GET(F_)
+DEBUGGER_SET(F_)
+DEBUGGER_GET(H_)
+DEBUGGER_SET(H_)
+DEBUGGER_GET(L_)
+DEBUGGER_SET(L_)
+
 DEBUGGER_GET(AF)
 DEBUGGER_SET(AF)
 DEBUGGER_GET(BC)
@@ -126,6 +195,58 @@ DEBUGGER_GET(DE)
 DEBUGGER_SET(DE)
 DEBUGGER_GET(HL)
 DEBUGGER_SET(HL)
+
+DEBUGGER_GET(AF_)
+DEBUGGER_SET(AF_)
+DEBUGGER_GET(BC_)
+DEBUGGER_SET(BC_)
+DEBUGGER_GET(DE_)
+DEBUGGER_SET(DE_)
+DEBUGGER_GET(HL_)
+DEBUGGER_SET(HL_)
+
+DEBUGGER_GET(SP)
+DEBUGGER_SET(SP)
+DEBUGGER_GET(PC)
+DEBUGGER_SET(PC)
+DEBUGGER_GET(IX)
+DEBUGGER_SET(IX)
+DEBUGGER_GET(IY)
+DEBUGGER_SET(IY)
+
+DEBUGGER_GET(I)
+DEBUGGER_SET(I)
+
+static libspectrum_dword get_R( void )
+{
+  return ( R7 & 0x80 ) | ( R & 0x7f );
+}
+
+static void set_R( libspectrum_dword value )
+{
+  R = R7 = value;
+}
+
+DEBUGGER_GET(IM)
+
+static void set_IM( libspectrum_dword value )
+{
+  if( value >= 0 && value <= 2 ) IM = value;
+}
+
+DEBUGGER_GET(IFF1)
+
+static void set_IFF1( libspectrum_dword value )
+{
+  IFF1 = !!value;
+}
+
+DEBUGGER_GET(IFF2)
+
+static void set_IFF2( libspectrum_dword value )
+{
+  IFF2 = !!value;
+}
 
 /* Set up the z80 emulation */
 void
@@ -140,10 +261,45 @@ z80_init( void )
 
   module_register( &z80_module_info );
 
+  debugger_system_variable_register( debugger_type_string, a_detail_string, get_A, set_A );
+  debugger_system_variable_register( debugger_type_string, b_detail_string, get_B, set_B );
+  debugger_system_variable_register( debugger_type_string, c_detail_string, get_C, set_C );
+  debugger_system_variable_register( debugger_type_string, d_detail_string, get_D, set_D );
+  debugger_system_variable_register( debugger_type_string, e_detail_string, get_E, set_E );
+  debugger_system_variable_register( debugger_type_string, f_detail_string, get_F, set_F );
+  debugger_system_variable_register( debugger_type_string, h_detail_string, get_H, set_H );
+  debugger_system_variable_register( debugger_type_string, l_detail_string, get_L, set_L );
+  
+  debugger_system_variable_register( debugger_type_string, a__detail_string, get_A_, set_A_ );
+  debugger_system_variable_register( debugger_type_string, b__detail_string, get_B_, set_B_ );
+  debugger_system_variable_register( debugger_type_string, c__detail_string, get_C_, set_C_ );
+  debugger_system_variable_register( debugger_type_string, d__detail_string, get_D_, set_D_ );
+  debugger_system_variable_register( debugger_type_string, e__detail_string, get_E_, set_E_ );
+  debugger_system_variable_register( debugger_type_string, f__detail_string, get_F_, set_F_ );
+  debugger_system_variable_register( debugger_type_string, h__detail_string, get_H_, set_H_ );
+  debugger_system_variable_register( debugger_type_string, l__detail_string, get_L_, set_L_ );
+  
   debugger_system_variable_register( debugger_type_string, af_detail_string, get_AF, set_AF );
   debugger_system_variable_register( debugger_type_string, bc_detail_string, get_BC, set_BC );
   debugger_system_variable_register( debugger_type_string, de_detail_string, get_DE, set_DE );
   debugger_system_variable_register( debugger_type_string, hl_detail_string, get_HL, set_HL );
+
+  debugger_system_variable_register( debugger_type_string, af__detail_string, get_AF_, set_AF_ );
+  debugger_system_variable_register( debugger_type_string, bc__detail_string, get_BC_, set_BC_ );
+  debugger_system_variable_register( debugger_type_string, de__detail_string, get_DE_, set_DE_ );
+  debugger_system_variable_register( debugger_type_string, hl__detail_string, get_HL_, set_HL_ );
+
+  debugger_system_variable_register( debugger_type_string, sp_detail_string, get_SP, set_SP );
+  debugger_system_variable_register( debugger_type_string, pc_detail_string, get_PC, set_PC );
+  debugger_system_variable_register( debugger_type_string, ix_detail_string, get_IX, set_IX );
+  debugger_system_variable_register( debugger_type_string, iy_detail_string, get_IY, set_IY );
+  
+  debugger_system_variable_register( debugger_type_string, i_detail_string, get_I, set_I );
+  debugger_system_variable_register( debugger_type_string, r_detail_string, get_R, set_R );
+
+  debugger_system_variable_register( debugger_type_string, im_detail_string, get_IM, set_IM );
+  debugger_system_variable_register( debugger_type_string, iff1_detail_string, get_IFF1, set_IFF1 );
+  debugger_system_variable_register( debugger_type_string, iff2_detail_string, get_IFF2, set_IFF2 );
 }
 
 /* Initalise the tables used to set flags */
