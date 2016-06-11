79a28466d73ffcfc8f1993dd1dc6ee05428bdfcf
Sergio Baldoví <serbalgi@gmail.com>
2016-06-11 18:05:42 +0000

    Make breakpoints on events honour lifetime.
    Legacy-ID: 5626

--- debugger/event.c
+++ debugger/event.c
@@ -88,7 +88,10 @@ void
 debugger_event( int event_code )
 {
   debugger_event_t event;
-  GSList *ptr;
+  debugger_breakpoint *bp;
+  GSList *ptr, *ptr_next;
+
+  int signal_breakpoints_updated = 0;
 
   if( event_code >= registered_events->len ) {
     ui_error( UI_ERROR_ERROR, "internal error: invalid debugger event %d",
@@ -98,16 +101,28 @@ debugger_event( int event_code )
 
   event = g_array_index( registered_events, debugger_event_t, event_code );
 
-  for( ptr = debugger_breakpoints; ptr; ptr = ptr->next ) {
-    debugger_breakpoint *bp = ptr->data;
+  for( ptr = debugger_breakpoints; ptr; ptr = ptr_next ) {
+
+    bp = ptr->data;
+    ptr_next = ptr->next;
+
     if( bp->type != DEBUGGER_BREAKPOINT_TYPE_EVENT ) continue;
 
     if( event_matches( &bp->value.event, event.type, event.detail ) &&
         debugger_breakpoint_trigger( bp ) ) {
       debugger_mode = DEBUGGER_MODE_HALTED;
       debugger_command_evaluate( bp->commands );
+
+      if( bp->life == DEBUGGER_BREAKPOINT_LIFE_ONESHOT ) {
+        debugger_breakpoints = g_slist_remove( debugger_breakpoints, bp );
+        libspectrum_free( bp );
+        signal_breakpoints_updated = 1;
+      }
     }
   }
+
+  if( signal_breakpoints_updated )
+      ui_breakpoints_updated();
 }
 
 /* Tidy-up function called at end of emulation */
