8a1afa39e88276ac414cba317e955a0a8fffe8e4
Sergio Baldoví <serbalgi@gmail.com>
2016-08-28 07:30:34 +0000

    Set the digit filter for text entry widgets on win32 UI.
    Legacy-ID: 5782

--- ui/win32/options-resource.pl
+++ ui/win32/options-resource.pl
@@ -59,9 +59,9 @@ foreach( @dialogs ) {
 	} elsif( $widget->{type} eq "Entry" ) {
 	    $y_label = $y + 2;
 	    $buffer .= sprintf "  LTEXT \"%s\",IDC_%s_LABEL_%s,5,$y_label,90,9\n",
-		$text, $optname, uc( $widget->{value} );
-	    $buffer .= sprintf "  EDITTEXT IDC_%s_%s,100,$y,85,13\n",
-		$optname, uc( $widget->{value} );
+	        $text, $optname, uc( $widget->{value} );
+	    $buffer .= sprintf "  EDITTEXT IDC_%s_%s,100,$y,85,13,ES_NUMBER\n",
+	        $optname, uc( $widget->{value} );
 	    $y += 14;
 	} elsif( $widget->{type} eq "Combo" ) {
 	    $y_label = $y + 2;
