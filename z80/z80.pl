7f70aed0a74539404b856dcb74b87255d6cad7eb
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2016-12-15 20:12:38 +0000

    Set MEMPTR after 0x3a LD A,(nnnn).

--- z80/z80.pl
+++ z80/z80.pl
@@ -751,10 +751,9 @@ LD
         } elsif( $src eq '(nnnn)' ) {
 	    print << "LD";
       {
-	libspectrum_word wordtemp;
-	wordtemp = readbyte(PC++);
-	wordtemp|= ( readbyte(PC++) << 8 );
-	A=readbyte(wordtemp);
+	z80.memptr.b.l = readbyte(PC++);
+	z80.memptr.b.h = readbyte(PC++);
+	A=readbyte(z80.memptr.w);
       }
 LD
         } elsif( $src eq '(REGISTER+dd)' ) {
