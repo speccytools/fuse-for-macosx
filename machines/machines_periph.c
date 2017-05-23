6912ce74e326e391873b16f1119cc37aef7ee87c
Fredrick Meunier <fredm@spamcop.net>
2017-05-23 14:21:23 +1000

    No Multiface 3 for 48k machines

--- machines/machines_periph.c
+++ machines/machines_periph.c
@@ -214,7 +214,6 @@ machines_periph_48( void )
   base_peripherals_48_128();
   periph_set_present( PERIPH_TYPE_FULLER, PERIPH_PRESENT_OPTIONAL );
   periph_set_present( PERIPH_TYPE_MULTIFACE_1, PERIPH_PRESENT_OPTIONAL );
-  periph_set_present( PERIPH_TYPE_MULTIFACE_3, PERIPH_PRESENT_OPTIONAL );
   periph_set_present( PERIPH_TYPE_ZXPRINTER, PERIPH_PRESENT_OPTIONAL );
   periph_set_present( PERIPH_TYPE_DIDAKTIK80, PERIPH_PRESENT_OPTIONAL );
   periph_set_present( PERIPH_TYPE_DISCIPLE, PERIPH_PRESENT_OPTIONAL );
