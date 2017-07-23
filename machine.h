4885cf9a107aeb287130c16b791ac996e8c51773
Sergio Baldoví <serbalgi@gmail.com>
2017-07-24 00:12:04 +0200

    Fix transposed description of AY-3-8912

--- machine.h
+++ machine.h
@@ -76,7 +76,7 @@ typedef struct fuse_machine_info {
 						  from a port which isn't
 						  attached to anything */
 
-  ayinfo ay;		/* The AY-8-3912 chip */
+  ayinfo ay;		/* The AY-3-8912 chip */
 
   specdrum_info specdrum; /* SpecDrum settings */
 
