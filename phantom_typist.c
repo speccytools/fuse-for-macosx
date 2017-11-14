4d18728bdb7c44c1d3cb1cc0ed0b57557521dc84
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2017-11-14 20:07:42 +0000

    Refactor phantom typist core loop.
    * Use named states.
    * Use a switch rather than multiple if blocks.

--- phantom_typist.c
+++ phantom_typist.c
@@ -25,8 +25,17 @@
 
 #include <libspectrum.h>
 
-static int phantom_typist_state = 0;
-static int next_phantom_typist_state = 1;
+typedef enum phantom_typist_state_t {
+  PHANTOM_TYPIST_STATE_INACTIVE,
+  PHANTOM_TYPIST_STATE_WAITING,
+  PHANTOM_TYPIST_STATE_LOAD,
+  PHANTOM_TYPIST_STATE_QUOTE1,
+  PHANTOM_TYPIST_STATE_QUOTE2,
+  PHANTOM_TYPIST_STATE_ENTER
+} phantom_typist_state_t;
+
+static phantom_typist_state_t phantom_typist_state = PHANTOM_TYPIST_STATE_INACTIVE;
+static phantom_typist_state_t next_phantom_typist_state = PHANTOM_TYPIST_STATE_WAITING;
 static int count = 0;
 static libspectrum_dword first_read = 100000;
 static libspectrum_byte keyboard_ports_read = 0x00;
@@ -37,73 +46,85 @@ phantom_typist_ula_read( libspectrum_word port )
   libspectrum_byte r = 0xff;
   libspectrum_byte high_byte = port >> 8;
 
-  if( phantom_typist_state == 1 && high_byte != 0xff ) {
-    switch( high_byte ) {
-      case 0xfe:
-        keyboard_ports_read |= 0x01;
-        break;
-      case 0xfd:
-        keyboard_ports_read |= 0x02;
-        break;
-      case 0xfb:
-        keyboard_ports_read |= 0x04;
-        break;
-      case 0xf7:
-        keyboard_ports_read |= 0x08;
-        break;
-      case 0xef:
-        keyboard_ports_read |= 0x10;
-        break;
-      case 0xdf:
-        keyboard_ports_read |= 0x20;
-        break;
-      case 0xbf:
-        keyboard_ports_read |= 0x40;
-        break;
-      case 0x7f:
-        keyboard_ports_read |= 0x80;
-        break;
-    }
+  switch( phantom_typist_state ) {
+    case PHANTOM_TYPIST_STATE_INACTIVE:
+      /* Do nothing */
+      break;
 
-    if( keyboard_ports_read == 0xff ) {
-      next_phantom_typist_state = 2;
-      count = 8;
-    }
-  }
+    case PHANTOM_TYPIST_STATE_WAITING:
+      switch( high_byte ) {
+        case 0xfe:
+          keyboard_ports_read |= 0x01;
+          break;
+        case 0xfd:
+          keyboard_ports_read |= 0x02;
+          break;
+        case 0xfb:
+          keyboard_ports_read |= 0x04;
+          break;
+        case 0xf7:
+          keyboard_ports_read |= 0x08;
+          break;
+        case 0xef:
+          keyboard_ports_read |= 0x10;
+          break;
+        case 0xdf:
+          keyboard_ports_read |= 0x20;
+          break;
+        case 0xbf:
+          keyboard_ports_read |= 0x40;
+          break;
+        case 0x7f:
+          keyboard_ports_read |= 0x80;
+          break;
+      }
 
-  if( phantom_typist_state == 2 && count == 0 && high_byte == 0xbf ) {
-    r &= ~0x08;
-    next_phantom_typist_state = 3;
-  }
+      if( keyboard_ports_read == 0xff ) {
+        next_phantom_typist_state = 2;
+        count = 8;
+      }
+      break;
 
-  if( phantom_typist_state == 3 ) {
-    switch( high_byte ) {
-      case 0x7f:
-        r &= ~0x02;
-        break;
-      case 0xdf:
+    case PHANTOM_TYPIST_STATE_LOAD:
+      if( count == 0 && high_byte == 0xbf ) {
+        r &= ~0x08;
+        next_phantom_typist_state = 3;
+      }
+      break;
+
+    case PHANTOM_TYPIST_STATE_QUOTE1:
+      switch( high_byte ) {
+        case 0x7f:
+          r &= ~0x02;
+          break;
+        case 0xdf:
+          r &= ~0x01;
+          break;
+      }
+      next_phantom_typist_state = 4;
+      count = 5;
+      break;
+
+    case PHANTOM_TYPIST_STATE_QUOTE2:
+      if( count == 0 ) {
+        switch( high_byte ) {
+          case 0x7f:
+            r &= ~0x02;
+            break;
+          case 0xdf:
+            r &= ~0x01;
+            break;
+        }
+        next_phantom_typist_state = 5;
+      }
+      break;
+
+    case PHANTOM_TYPIST_STATE_ENTER:
+      if( high_byte == 0xbf ) {
         r &= ~0x01;
-        break;
-    }
-    next_phantom_typist_state = 4;
-    count = 5;
-  }
-
-  if( phantom_typist_state == 4 && count == 0 ) {
-    switch( high_byte ) {
-      case 0x7f:
-        r &= ~0x02;
-        break;
-      case 0xdf:
-        r &= ~0x01;
-        break;
-    }
-    next_phantom_typist_state = 5;
-  }
-
-  if( phantom_typist_state == 5 && high_byte == 0xbf ) {
-    r &= ~0x01;
-    next_phantom_typist_state = 0;
+        next_phantom_typist_state = 0;
+      }
+      break;
   }
 
   return r;
