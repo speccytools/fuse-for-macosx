8548540f6224f3a61e16c601bc4d1c9a7348c7fe
Philip Kendall <philip-fuse@shadowmagic.org.uk>
2018-10-14 22:58:26 +0100

    Fix Kempston mouse Y values on GTK+ 3.x.
    Broken by previous change, fixed by changing all Kempston mouse code to
    be based off gtkui_window; also encapsulate things a bit better while
    we're at it.

--- ui/gtk/gtkmouse.c
+++ ui/gtk/gtkmouse.c
@@ -40,20 +40,15 @@
 
 static GdkCursor *nullpointer = NULL;
 
+/* The widget we base our events, grabs, warping etc on */
+static GtkWidget *mouse_widget = NULL;
+
 static void
 gtkmouse_reset_pointer( void )
 {
   /* Ugh. GDK doesn't have its own move-pointer function :-|
-   * Framebuffer users and win32 users will have to make their own
-   * arrangements here.
-   *
-   * For Win32, use SetCursorPos() -- see sdpGtkWarpPointer() at
-   * http://k3d.cvs.sourceforge.net/k3d/projects/sdplibs/sdpgtk/sdpgtkutility.cpp?view=markup
-   */
-  GtkWidget *widget;
-  GdkWindow *window;
 
-  /* The logic here is a bit hairy:
+     The logic here is a bit hairy:
 
      * On GTK+ 2.x, we warp relative to the drawing area
      * On GTK+ 3.x on X11, we warp relative to the top-level window
@@ -61,29 +56,21 @@ gtkmouse_reset_pointer( void )
        segfault (see bug #435)
    */
 
-#if GTK_CHECK_VERSION( 3, 0, 0 )
+  GdkWindow *window;
 
 #ifdef GDK_WINDOWING_WAYLAND
   GdkDisplay *display = gdk_display_get_default();
   if( GDK_IS_WAYLAND_DISPLAY( display ) ) return;
 #endif                /* #ifdef GDK_WINDOWING_WAYLAND */
 
-  widget = gtkui_window;
-
-#else                 /* #if GTK_CHECK_VERSION( 3, 0, 0 ) */
-
-  widget = gtkui_drawing_area;
-
-#endif                /* #if GTK_CHECK_VERSION( 3, 0, 0 ) */
-
-  window = gtk_widget_get_window( widget );
+  window = gtk_widget_get_window( mouse_widget );
   XWarpPointer( GDK_WINDOW_XDISPLAY( window ), None, 
                 GDK_WINDOW_XID( window ), 0, 0, 0, 0, 128, 128 );
 }
 
-gboolean
-gtkmouse_position( GtkWidget *widget GCC_UNUSED,
-                   GdkEventMotion *event, gpointer data GCC_UNUSED )
+static gboolean
+motion_event( GtkWidget *widget GCC_UNUSED, GdkEventMotion *event,
+              gpointer data GCC_UNUSED )
 {
   if( !ui_mouse_grabbed ) return TRUE;
 
@@ -93,9 +80,9 @@ gtkmouse_position( GtkWidget *widget GCC_UNUSED,
   return TRUE;
 }
 
-gboolean
-gtkmouse_button( GtkWidget *widget GCC_UNUSED, GdkEventButton *event,
-		 gpointer data GCC_UNUSED )
+static gboolean
+button_event( GtkWidget *widget GCC_UNUSED, GdkEventButton *event,
+	      gpointer data GCC_UNUSED )
 {
   if( event->type == GDK_BUTTON_PRESS || event->type == GDK_2BUTTON_PRESS
       || event->type == GDK_3BUTTON_PRESS )
@@ -103,6 +90,25 @@ gtkmouse_button( GtkWidget *widget GCC_UNUSED, GdkEventButton *event,
   else
     ui_mouse_button( event->button, 0 );
   return TRUE;
+}
+
+void
+gtkmouse_init( void )
+{
+#if GTK_CHECK_VERSION( 3, 0, 0 )
+  mouse_widget = gtkui_window;
+#else                 /* #if GTK_CHECK_VERSION( 3, 0, 0 ) */
+  mouse_widget = gtkui_drawing_area;
+#endif                /* #if GTK_CHECK_VERSION( 3, 0, 0 ) */
+
+  gtk_widget_add_events( GTK_WIDGET( mouse_widget ),
+    GDK_POINTER_MOTION_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK );
+  g_signal_connect( G_OBJECT( mouse_widget ), "motion-notify-event",
+		    G_CALLBACK( motion_event ), NULL );
+  g_signal_connect( G_OBJECT( mouse_widget ), "button-press-event",
+		    G_CALLBACK( button_event ), NULL );
+  g_signal_connect( G_OBJECT( mouse_widget ), "button-release-event",
+		    G_CALLBACK( button_event ), NULL );
 }
 
 int
@@ -113,7 +119,7 @@ ui_mouse_grab( int startup )
 
   if( startup ) return 0;
 
-  window = gtk_widget_get_window( gtkui_drawing_area );
+  window = gtk_widget_get_window( mouse_widget );
 
 #if !GTK_CHECK_VERSION( 3, 0, 0 )
 
@@ -171,7 +177,7 @@ ui_mouse_release( int suspend GCC_UNUSED )
   GdkDeviceManager *device_manager;
   GdkDevice *pointer;
 
-  display = gtk_widget_get_display( gtkui_drawing_area );
+  display = gtk_widget_get_display( mouse_widget );
   device_manager = gdk_display_get_device_manager( display );
   pointer = gdk_device_manager_get_client_pointer( device_manager );
 
