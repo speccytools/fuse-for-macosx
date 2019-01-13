speccytools/fuse-for-macosx

commit d9ce57e08d19030c31e9b4eab7ee8170ef923510

Improve error message when opening a directory
Show helpful error message if a directory is specified on the command
line (thanks, Philip Kendall) (Alberto Garcia and Sergio Baldoví).

compat/unix/file.c
@@ -39,6 +39,18 @@ const compat_fd COMPAT_FILE_OPEN_FAILED = NULL;
 compat_fd
 compat_file_open( const char *path, int write )
 {
+  struct stat statbuf;
+
+  if( stat( path, &statbuf ) ) {
+    return NULL;
+  }
+
+  /* Check file type */
+  if( !S_ISREG( statbuf.st_mode ) ) {
+    errno = EINVAL;
+    return NULL;
+  }
+
   return fopen( path, write ? "wb" : "rb" );
 }
 

commit 63969a7ab1406fe76a2b2e02a4079f9451c33edd

Check if a file is a regular file only when reading
Regression since [d9ce57e0].

compat/unix/file.c
@@ -41,14 +41,16 @@ compat_file_open( const char *path, int write )
 {
   struct stat statbuf;
 
-  if( stat( path, &statbuf ) ) {
-    return NULL;
-  }
+  if( !write ) {
+    if( stat( path, &statbuf ) ) {
+      return NULL;
+    }
 
-  /* Check file type */
-  if( !S_ISREG( statbuf.st_mode ) ) {
-    errno = EINVAL;
-    return NULL;
+    /* Check file type */
+    if( !S_ISREG( statbuf.st_mode ) ) {
+      errno = EINVAL;
+      return NULL;
+    }
   }
 
   return fopen( path, write ? "wb" : "rb" );